41dc8cbe613ccf67c65e5ded1cdbf5e12952e318
Taylor Goolsby <taylorgoolsby@users.noreply.github.com>
2021-07-26 20:54:59 -0700

    Using new Functional Schema Directives

--- __tests__/main-test.ts
+++ __tests__/main-test.ts
@@ -1,16 +1,13 @@
-import { validateSchema, introspectionFromSchema, execute } from 'graphql'
-import { makeExecutableSchema, transformSchema } from 'graphql-tools'
-import gql from 'graphql-tag'
-import {
-  PrivateDirective,
-  privateTransform,
-  privateDirectiveDeclaration,
-} from '../src'
+import { makeExecutableSchema, printSchemaWithDirectives } from 'graphql-tools'
+import privateDirective from '../src'
+
+const {
+  privateDirectiveTypeDefs,
+  privateDirectiveTransform,
+} = privateDirective('private')
 
 test('control', async () => {
   const typeDefs = `
-    ${privateDirectiveDeclaration}
-
     type User {
       userId: Int
       post: Post
@@ -26,131 +23,55 @@ test('control', async () => {
       post: Post
     }
   `
-
-  let schema = makeExecutableSchema({
-    typeDefs,
-    schemaDirectives: {
-      private: PrivateDirective,
-    },
-    resolvers: {
-      Query: {
-        user: () => ({}),
-        post: () => ({}),
-      },
-      User: {
-        userId: () => 1,
-        post: () => ({}),
-      },
-      Post: {
-        postId: () => 1,
-        user: () => ({}),
-      },
-    },
-  })
-  schema = transformSchema(schema, [privateTransform(schema)])
-
-  const errors = validateSchema(schema)
-  expect(errors.length).toBe(0)
-
-  const query = gql`
-    query {
-      user {
-        userId
-        post {
-          postId
-        }
-      }
-      post {
-        postId
-        user {
-          userId
-        }
-      }
-    }
-  `
-  const result: any = await execute(schema, query)
-  expect(result).toEqual({
-    data: {
-      post: { postId: 1, user: { userId: 1 } },
-      user: { post: { postId: 1 }, userId: 1 },
-    },
-  })
-  // console.log('intro', JSON.stringify(result))
+  const expected = `schema {
+  query: Query
+}
+type User {
+  userId: Int
+  post: Post
+}
+type Post {
+  postId: Int
+  user: User
+}
+type Query {
+  user: User
+  post: Post
+}
+directive @private on OBJECT | FIELD_DEFINITION`
+  runTest(typeDefs, expected)
 })
 
 test('private object', async () => {
   const typeDefs = `
-    ${privateDirectiveDeclaration}
-
     type User @private {
       userId: Int
       post: Post
     }
-
     type Post {
       postId: Int
       user: User
     }
-
     type Query {
       user: User
       post: Post
     }
   `
-
-  let schema = makeExecutableSchema({
-    typeDefs,
-    schemaDirectives: {
-      private: PrivateDirective,
-    },
-    resolvers: {
-      Query: {
-        user: () => ({}),
-        post: () => ({}),
-      },
-      User: {
-        userId: () => 1,
-        post: () => ({}),
-      },
-      Post: {
-        postId: () => 1,
-        user: () => ({}),
-      },
-    },
-  })
-  schema = transformSchema(schema, [privateTransform(schema)])
-
-  const errors = validateSchema(schema)
-  expect(errors.length).toBe(0)
-
-  // const intro = introspectionFromSchema(schema)
-  // console.log(JSON.stringify(intro))
-
-  const query = gql`
-    query {
-      user {
-        userId
-        post {
-          postId
-        }
-      }
-      post {
-        postId
-        user {
-          userId
-        }
-      }
-    }
-  `
-  const result: any = await execute(schema, query)
-  expect(result).toEqual({ data: { post: { postId: 1 } } })
-  // console.log('intro', JSON.stringify(result))
+  const expected = `schema {
+  query: Query
+}
+type Post {
+  postId: Int
+}
+type Query {
+  post: Post
+}
+directive @private on OBJECT | FIELD_DEFINITION`
+  runTest(typeDefs, expected)
 })
 
 test('private field', async () => {
   const typeDefs = `
-    ${privateDirectiveDeclaration}
-
     type User {
       userId: Int @private
       post: Post
@@ -166,120 +87,34 @@ test('private field', async () => {
       post: Post
     }
   `
-
-  let schema = makeExecutableSchema({
-    typeDefs,
-    schemaDirectives: {
-      private: PrivateDirective,
-    },
-    resolvers: {
-      Query: {
-        user: () => ({}),
-        post: () => ({}),
-      },
-      User: {
-        userId: () => 1,
-        post: () => ({}),
-      },
-      Post: {
-        postId: () => 1,
-        user: () => ({}),
-      },
-    },
-  })
-  schema = transformSchema(schema, [privateTransform(schema)])
-
-  const errors = validateSchema(schema)
-  expect(errors.length).toBe(0)
-
-  const query = gql`
-    query {
-      user {
-        userId
-        post {
-          postId
-        }
-      }
-      post {
-        postId
-        user {
-          userId
-        }
-      }
-    }
-  `
-  const result: any = await execute(schema, query)
-  expect(result).toEqual({
-    data: {
-      post: { postId: 1, user: null },
-      user: { post: { postId: 1 }, userId: null },
-    },
-  })
-  // console.log('intro', JSON.stringify(result))
+  const expected = `schema {
+  query: Query
+}
+type User {
+  post: Post
+}
+type Post {
+  postId: Int
+  user: User
+}
+type Query {
+  user: User
+  post: Post
+}
+directive @private on OBJECT | FIELD_DEFINITION`
+  runTest(typeDefs, expected)
 })
 
-test('mixed', async () => {
-  const typeDefs = `
-    ${privateDirectiveDeclaration}
-
-    type User @private {
-      userId: Int
-      post: Post
-    }
-
-    type Post {
-      postId: Int @private
-      user: User
-    }
-
-    type Query {
-      user: User
-      post: Post
-    }
-  `
-
+function runTest(typeDefs: string, expected: string) {
   let schema = makeExecutableSchema({
-    typeDefs,
-    schemaDirectives: {
-      private: PrivateDirective,
-    },
-    resolvers: {
-      Query: {
-        user: () => ({}),
-        post: () => ({}),
-      },
-      User: {
-        userId: () => 1,
-        post: () => ({}),
-      },
-      Post: {
-        postId: () => 1,
-        user: () => ({}),
-      },
-    },
+    typeDefs: [privateDirectiveTypeDefs, typeDefs],
   })
-  schema = transformSchema(schema, [privateTransform(schema)])
+  schema = privateDirectiveTransform(schema)
+  const answer = printSchemaWithDirectives(schema)
 
-  const errors = validateSchema(schema)
-  expect(errors.length).toBe(0)
+  if (answer !== expected) {
+    console.log(answer)
+  }
 
-  const query = gql`
-    query {
-      user {
-        userId
-        post {
-          postId
-        }
-      }
-      post {
-        postId
-        user {
-          userId
-        }
-      }
-    }
-  `
-  const result: any = await execute(schema, query)
-  expect(result).toEqual({ data: { post: { postId: null } } })
-  // console.log('intro', JSON.stringify(result))
-})
+  expect(answer).toEqual(expected)
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-// export * from './applyPrivateTransform'
-export * from './PrivateDirective'
-export * from './privateTransform'
-export function customDirectiveDeclaration(
-  customDirectiveName: string
-): string {
-  return `directive @${customDirectiveName} on OBJECT | FIELD_DEFINITION`
+import { mapSchema, getDirectives, MapperKind } from 'graphql-tools'
+import { GraphQLSchema } from 'graphql'
+
+export default function privateDirective(directiveName: string) {
+  return {
+    privateDirectiveTypeDefs: `directive @private on OBJECT | FIELD_DEFINITION`,
+    privateDirectiveTransform: (schema: GraphQLSchema) =>
+      mapSchema(schema, {
+        [MapperKind.TYPE]: (type) => {
+          const typeDirectives = getDirectives(schema, type)
+          if (typeDirectives.private) {
+            return null
+          } else {
+            return type
+          }
+        },
+        [MapperKind.OBJECT_FIELD]: (fieldConfig, fieldName, typeName) => {
+          const fieldDirectives = getDirectives(schema, fieldConfig)
+          if (fieldDirectives.private) {
+            return null
+          } else {
+            return fieldConfig
+          }
+        },
+      }),
+  }
 }
-export const privateDirectiveDeclaration = customDirectiveDeclaration('private')
